01e7cd434909fe2c0f097195e100dcca87b05231
Paul Hammant <phammant@hedgeserv.com>
2016-10-18 07:59:10 -0400

    static files demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+
 import static spark.Spark.*;
 public class Main {
 	public static void main(String[] args) {
+        staticFiles.location("/static");
+        before("*", (request, response) -> {
+            response.type("text/html");
+        });
         get("/hello", (req, res) -> "Hello World");
     }
 }
